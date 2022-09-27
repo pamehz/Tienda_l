f5bbab8048db81eed8170a965fbdb5825246452a
Pamela <Pamela@DESKTOP-18B5RU3>
2022-09-26 19:03:17 -0600

    Semana 1 y 2 en un commit por error

---
+++ src/main/java/com/tienda_l/TiendaLApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLApplication.class, args);
+	}
+
+}
